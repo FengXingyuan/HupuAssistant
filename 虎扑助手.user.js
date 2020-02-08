b7dfd7c0dc9837e595a288c0fb9795eea8a13259
FengXingyuan <landswimmer@hotmail.com>
2020-02-08 22:52:49 +0800

    v0.5.1增加了图片的预加载

--- 虎扑助手.user.js
+++ 虎扑助手.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         虎扑助手
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
+// @version      0.5
 // @description  在浏览虎扑时可一键查看所有图片和视频
 // @author       landswimmer
 // @match        https://bbs.hupu.com/*.html
@@ -12,67 +12,24 @@
 
 (function () {
     'use strict';
-    //第一部分，布局相关
-        let picModeBtn = document.createElement("div");
-        picModeBtn.innerHTML = "看图";
-        picModeBtn.style.display = "flex";
-        picModeBtn.style.justifyContent = "center";
-        picModeBtn.style.alignItems = "center";
-        picModeBtn.style.position = "fixed";
-        picModeBtn.style.right = "2%";
-        picModeBtn.style.bottom = "50px";
-        picModeBtn.style.width = "48px";
-        picModeBtn.style.height = "48px";
-        picModeBtn.style.border = "1px solid #999999";
-        picModeBtn.style.borderRadius = "3px";
-        picModeBtn.style.cursor = "pointer";
-
-        let picModeDiv = document.createElement("div");
-        picModeDiv.style.position = "fixed";
-        picModeDiv.style.display = "flex";
-        picModeDiv.style.justifyContent = "center";
-        picModeDiv.style.alignItems = "center";
-        picModeDiv.style.top = "0"
-        picModeDiv.style.zIndex = "999"
-        picModeDiv.style.visibility = "hidden";
-        picModeDiv.style.width = "100vw";
-        picModeDiv.style.height = "100vh";
-        picModeDiv.style.backgroundColor = "#222222";
-
-        let placeHolder = document.createElement("div");
-        placeHolder.innerHTML = "本帖没有图片哦";
-        placeHolder.style.display = "none";
-        placeHolder.style.color = "white";
-
-        let pic = document.createElement("img");
-        pic.style.display = "none";
-        pic.style.maxWidth = "80vw";
-        pic.style.maxHeight = "90vh";
-        pic.style.cursor = "zoom-in";
-
-        let mp4 = document.createElement("video");
-        mp4.controls = "true";
-        mp4.style.display = "none";
-        mp4.style.maxHeight = "90vh";
-        mp4.style.maxWidth = "80vw";  
-
-    //第二部分，逻辑相关
+    //第一部分，逻辑相关
     let imgList = [];
-    let i = 0;
+    let imgBuffer = [];
     let pageIndex = 1;
     let re = /src='.*?'/g;
-    let baseUrl = 'https://m.hupu.com/api/v1/bbs-thread-frontend/' + window.location.href.substr(21, window.location.href.length - 26) + '?page=';
-    //提取帖子id
+    let baseUrl = 'https://m.hupu.com/api/v1/bbs-thread-frontend/' + window.location.href.substr(21, window.location.href.length - 26) + '?page=';//提取帖子id
 
     function dataHandler(eachReply) {
         let reResult = eachReply.content.match(re);
-        //console.log(reResult);
         if (reResult) {
             reResult.forEach(rawSrc => {
-                if(rawSrc.match(/\.mp4/)){
+                if (rawSrc.match(/\.mp4/g)) {
                     imgList.push(rawSrc.substr(5, rawSrc.length - 6).replace(/\\/g, ""));
-                }else{
-                    imgList.push(rawSrc.match(/src='.*?\?/g)[0].substr(5, rawSrc.length - 6).replace(/\\/g, ""));
+                } else if (rawSrc.match(/\?/g)) {
+                    imgList.push(rawSrc.match(/src=.*?\?/g)[0].replace(/\\/g, "").substr(5, rawSrc.length - 7));
+                    let bufferNode = new Image();
+                    bufferNode.src = rawSrc.match(/src=.*?\?/g)[0].replace(/\\/g, "").substr(5, rawSrc.length - 7);
+                    imgBuffer.push(bufferNode);
                 }
             });
         }
@@ -80,11 +37,6 @@
 
     let details = {
         method: "GET",
-        headers: {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-encoding": "gzip, deflate, br",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
-        },
         onload: (e) => {
             let responseData = JSON.parse(e.response).data;
             if (pageIndex === 1) {
@@ -97,84 +49,194 @@
             }
         }
     }
+
     function sendRequest() {
         details.url = baseUrl + pageIndex;
         GM_xmlhttpRequest(details);
     }
+
     sendRequest();
 
-    //请求第一页数据
-    //处理第一页数据
-    //读取所有的content,并用正则表达式匹配当中的图片链接
-    //去除链接当中的反斜杠
-    //将链接放入数组当中
-    //读取总页数
-    //如果有多页，循环请求处理剩余页面
+    //第二部分，布局相关
 
-    function keyDownHandler(e) {
-        if (picModeDiv.style.visibility === "visible") {
-            if (e.keyCode === 39) {
-                if (i < imgList.length - 1) {
-                    i++;
-                    if(imgList[i].match(/\.mp4/)){
-                        mp4.src = imgList[i];
-                        pic.style.display = "none";
-                        mp4.style.display = "initial";                      
-                    }else{
-                        pic.src = imgList[i];
-                        mp4.style.display = "none";
-                        pic.style.display = "initial";                       
-                    }
-                }
+    let picModeBtn = document.createElement("div");
+    picModeBtn.innerHTML = "看图";
+    picModeBtn.style.display = "flex";
+    picModeBtn.style.justifyContent = "center";
+    picModeBtn.style.alignItems = "center";
+    picModeBtn.style.position = "fixed";
+    picModeBtn.style.right = "calc(2% + 4px)";
+    picModeBtn.style.bottom = "50px";
+    picModeBtn.style.width = "40px";
+    picModeBtn.style.height = "40px";
+    picModeBtn.style.border = "1px solid #999999";
+    picModeBtn.style.borderRadius = "3px";
+    picModeBtn.style.cursor = "pointer";
+
+    let picModeDiv = document.createElement("div");
+    picModeDiv.id = "insertDiv";
+    picModeDiv.style.position = "fixed";
+    picModeDiv.style.display = "flex";
+    picModeDiv.style.justifyContent = "center";
+    picModeDiv.style.alignItems = "center";
+    picModeDiv.style.top = "0"
+    picModeDiv.style.zIndex = "1000"
+    picModeDiv.style.width = "100vw";
+    picModeDiv.style.height = "100vh";
+    picModeDiv.style.backgroundColor = "#222222";
+
+    let placeHolder = document.createElement("div");
+    placeHolder.innerHTML = "本帖没有图片哦";
+    placeHolder.style.display = "none";
+    placeHolder.style.color = "white";
+
+    let pic = document.createElement("img");
+    pic.style.display = "none";
+    pic.style.maxWidth = "100vw";
+    pic.style.maxHeight = "100vh";
+
+    let mp4 = document.createElement("video");
+    mp4.controls = "true";
+    mp4.style.display = "none";
+    mp4.style.maxHeight = "100vh";
+    mp4.style.maxWidth = "100vw";
+
+    let touchPadLeft = document.createElement("div");
+    touchPadLeft.style.position = "fixed";
+    touchPadLeft.style.visibility = "hidden";
+    touchPadLeft.style.top = "calc(50vh - 25px)";
+    touchPadLeft.style.left = "20px";
+    touchPadLeft.style.height = "0";
+    touchPadLeft.style.width = "0";
+    touchPadLeft.style.borderWidth = "25px";
+    touchPadLeft.style.borderStyle = "solid";
+    touchPadLeft.style.borderTopColor = "transparent";
+    touchPadLeft.style.borderRightColor = "rgba(128,128,128,0.8)";
+    touchPadLeft.style.borderBottomColor = "transparent";
+    touchPadLeft.style.borderLeftColor = "transparent";
+    touchPadLeft.style.mixBlendMode = "difference";
+    touchPadLeft.style.cursor = "pointer";
+
+    let touchPadRight = document.createElement("div");
+    touchPadRight.style.position = "fixed";
+    touchPadRight.style.visibility = "hidden";
+    touchPadRight.style.top = "calc(50vh - 25px)";
+    touchPadRight.style.right = "20px";
+    touchPadRight.style.height = "0";
+    touchPadRight.style.width = "0";
+    touchPadRight.style.borderWidth = "25px";
+    touchPadRight.style.borderStyle = "solid";
+    touchPadRight.style.borderTopColor = "transparent";
+    touchPadRight.style.borderRightColor = "transparent";
+    touchPadRight.style.borderBottomColor = "transparent";
+    touchPadRight.style.borderLeftColor = "rgba(128,128,128,0.8)";
+    touchPadRight.style.mixBlendMode = "difference";
+    touchPadRight.style.cursor = "pointer";
+
+    let closeBtn = document.createElement("div");
+    closeBtn.innerHTML = "X";
+    closeBtn.style.position = "fixed";
+    closeBtn.style.right = "30px";
+    closeBtn.style.top = "20px";
+    closeBtn.style.color = "rgba(128,128,128,0.8)";
+    closeBtn.style.mixBlendMode = "difference";
+    closeBtn.style.fontSize = "30px";
+    closeBtn.style.cursor = "pointer";
+
+    let fragment = document.createDocumentFragment();
+    let rawFragment = document.createDocumentFragment();
+    picModeDiv.appendChild(pic);
+    picModeDiv.appendChild(placeHolder);
+    picModeDiv.appendChild(mp4);
+    picModeDiv.appendChild(touchPadLeft);
+    picModeDiv.appendChild(touchPadRight);
+    picModeDiv.appendChild(closeBtn);
+    fragment.appendChild(picModeDiv);
+
+    //第三部分，交互控制相关
+    let i = 0;
+    function imgSwitch() {
+        if (imgList[i].match(/\.mp4/)) {
+            mp4.src = imgList[i];
+            mp4.style.display = "initial";
+            pic.style.display = "none";
+        } else {
+            pic.src = imgList[i];
+            pic.style.display = "initial";
+            mp4.style.display = "none";
+        }
+        if (i == 0) {
+            touchPadLeft.style.visibility = "hidden";
+        } else {
+            touchPadLeft.style.visibility = "visible";
+        }
+        if (i == imgList.length - 1) {
+            touchPadRight.style.visibility = "hidden";
+        } else {
+            touchPadRight.style.visibility = "visible";
+        }
+    }
+
+    document.onkeydown = (e) => {
+        if (rawFragment) {
+            if (e.keyCode === 39 && (i < imgList.length - 1)) {
+                i++;
+                imgSwitch();
             }
-            if (e.keyCode === 37) {
-                if (i > 0) {
-                    i--;
-                    if(imgList[i].match(/\.mp4/)){
-                        mp4.src = imgList[i];
-                        mp4.style.display = "initial";
-                        pic.style.display = "none";
-                    }else{
-                        pic.src = imgList[i];
-                        pic.style.display = "initial";
-                        mp4.style.display = "none";
-                    }
-                }
+            if (e.keyCode === 37 && i > 0) {
+                i--;
+                imgSwitch();
             }
             if (e.keyCode === 27) {
+                placeHolder.style.display = "none";
+                pic.style.display = "none";
+                mp4.style.display = "none";
+                touchPadRight.style.visibility = "hidden";
+                touchPadLeft.style.visibility = "hidden";
                 if (imgList.length) {
                     i = 0;
                     pic.src = imgList[i];
                 }
-                picModeDiv.style.visibility = "hidden";
+                fragment.appendChild(document.getElementById("insertDiv"));
+                document.body.appendChild(rawFragment);
             }
         }
     }
-
-    document.onkeydown = keyDownHandler;
-
+    touchPadLeft.onclick = () => {
+        if (i > 0) {
+            i--;
+            imgSwitch();
+        }
+    }
+    touchPadRight.onclick = () => {
+        if (i < imgList.length - 1) {
+            i++;
+            imgSwitch();
+        }
+    }
+    closeBtn.onclick = () => {
+        placeHolder.style.display = "none";
+        pic.style.display = "none";
+        mp4.style.display = "none";
+        touchPadRight.style.visibility = "hidden";
+        touchPadLeft.style.visibility = "hidden";
+        if (imgList.length) {
+            i = 0;
+            pic.src = imgList[i];
+        }
+        fragment.appendChild(document.getElementById("insertDiv"));
+        document.body.appendChild(rawFragment);
+    }
     picModeBtn.onclick = function () {
         imgList = [...new Set(imgList)];//抄袭代码
+        rawFragment.appendChild(document.getElementsByClassName("hp-wrap details")[0]);
         if (imgList.length) {
-            if(imgList[i].match(/\.mp4/)){
-                mp4.src = imgList[i];
-                mp4.style.display = "initial";
-            }else{
-                pic.src = imgList[i];
-                pic.style.display = "initial";
-            }           
+            imgSwitch();
         } else {
             placeHolder.style.display = "initial";
         }
-        picModeDiv.style.visibility = "visible";
+        document.body.appendChild(fragment);
     }
 
-    let bodyNode = document.body;
-    let fragment = document.createDocumentFragment();
-    picModeDiv.appendChild(pic);
-    picModeDiv.appendChild(placeHolder);
-    picModeDiv.appendChild(mp4);
-    fragment.appendChild(picModeBtn);
-    fragment.appendChild(picModeDiv);
-    bodyNode.appendChild(fragment);
+    document.body.appendChild(picModeBtn);
 })();
